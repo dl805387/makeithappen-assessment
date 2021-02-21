34ae8fdd27882df1d75d2380181b664b0d978549
dl805387 <59711497+dl805387@users.noreply.github.com>
2021-02-21 01:16:10 -0500

    initial setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express(); // create express app
+
+// add middleware
+//app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.send("This is from express.js");
+});
+
+// start express server on port 5000
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
